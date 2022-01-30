02c81918dab4d76916b265b39a3025b6d63d922d
Cbastn <pro2j.lol@gmail.com>
2022-01-30 13:46:06 -0700

    test: directory and file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-argc=($@)
 address=$1
 user=$2
 pass=$3
+search_dir=$4
 
-for (( c=3; c<$#; c++ ))
-do
-   echo 'Running cypher file:' ${argc[$c]}
-   #cypher-shell -a $address -u $user -p $pass -f ${argc[$c]}
-done
+if [[ -d $search_dir ]]; then
+   for file in "$search_dir"/*
+   do
+#    cypher-shell -a $address -u $user -p $pass -f $file
+   echo "$file"
+   done
+elif [[ -f $search_dir ]]; then
+#    cypher-shell -a $address -u $user -p $pass -f $file
+   echo "$file"
+else
+   echo not valid
+   exit 1
+fi
